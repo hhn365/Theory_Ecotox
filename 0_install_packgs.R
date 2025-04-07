035f920846de27ca5a939bcd6e59259d5b4192c5
Ralf B. Schäfer <ralfs@macbookpro.speedport.ip>
2025-04-07 10:51:49 +0200

    Installation updated

--- 0_install_packgs.R
+++ 0_install_packgs.R
@@ -1,7 +1,10 @@
+
+## Important notice - read before you run the code 
+## You need to manually install JAGS on your computer: https://sourceforge.net/projects/mcmc-jags/files/
+## This means you need to download and install locally
+## If you do not have JAGS installed, R packages will not work
+
 pkgs <- readLines(file("https://raw.githubusercontent.com/hhn365/Theory_Ecotox/refs/heads/main/Lecture_01_Introduction/installed_pkgs.txt", "r"))
 str(pkgs)
 install.packages(pkgs)
 BiocManager::install("DESeq2")
-
-## You need to manually install JAGS on your computer: https://sourceforge.net/projects/mcmc-jags/files/
-## This means you need to download and install locally 
