89e9095173e529e2539f9582ff5c7619f393f787
AndreGergs <andre.gergs@bayer.com>
2025-05-28 16:13:29 +0200

    Add files via upload

---
+++ Lecture_07_TKTD/Exercise_8_onsite.R
@@ -0,0 +1,53 @@
+#Exercise 6
+# Replace the bioaccumulation model by the one-parameter function for the scaled internal concentration Ci.
+# Compare modeling results for the scaled internal concentration with the ones obtained from Exercise 4
+  
+  
+#packages, settings
+library(deSolve)
+par(mfrow=c(2,2))
+
+#toxicokinetic function
+F_tk <- function(t, y,  pars, input) {
+  with(as.list(c(y, pars)), {
+    #exposure concentration
+    Cw     <- input(t)
+    #damage
+    dD <- kd*(Cw - D)
+    #hazard
+    dH <- b*max(D-z,0)+hb
+    
+    return(list(c(dD,dH))) 
+  })
+}
+
+
+#Inputs
+conc <- read.csv("exposure_scenario.csv", header=FALSE, sep=",", skip=1)
+t <- conc[,1]           #time vector
+
+#loop start
+for (i in 1:2){
+
+    Cw <- approxfun(t, conc[,i+1],  rule = 2, method = "constant") #interpolation
+    
+    #Initial state variables
+    ini=c(D=0, H=0)
+    
+    #parameters
+    pars<-c(kd =2.001e-02,
+            b=5.009e-02,
+            z=1.123e-01,
+            hb=0 #4.851e-04
+            
+            ) # 0.0166
+    
+    #call function, collect outputs
+    out<- lsoda(y = ini,times = t, func = F_tk, pars, input=Cw)
+    
+    
+    #plot results
+    plot(out[,1], out[,2], type="l", lty= 1, lwd=2, col="orange", xlab = "Time [h]", ylab = "Damage [g/kg]", ylim=c(0, 0.25))
+    plot(out[,1], exp(- out[,3]), type="l", lty= 1, lwd=2, col="red", xlab = "Time [h]", ylab = "survival [g/kg]", ylim=c(0, 1))
+    
+}  #loop end
