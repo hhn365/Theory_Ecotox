82bef680a2ac5bdff4f35e94228d767d7b6432d4
Ralf B. Schäfer <ralfs@MacBook-Pro-von-Ralf.local>
2026-04-07 15:56:47 +0200

    code adjusted

--- 0_install_packgs.R
+++ 0_install_packgs.R
@@ -4,7 +4,7 @@
 ## This means you need to download and install locally
 ## If you do not have JAGS installed, R packages will not work
 
-pkgs <- readLines(file("https://raw.githubusercontent.com/hhn365/Theory_Ecotox/refs/heads/main/Lecture_01_Introduction/installed_pkgs.txt", "r"))
+pkgs <- readLines(file("https://raw.githubusercontent.com/hhn365/Theory_Ecotox/refs/heads/main/Lecture_Introduction/installed_pkgs.txt", "r"))
 str(pkgs)
 install.packages(pkgs)
 BiocManager::install("DESeq2")
