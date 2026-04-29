7429514fd89e422b99c90f329a38a63ecaee4acb
Ralf B. Schäfer <ralfs@dhcp-wlce227090.wlan.zim.uni-duisburg-essen.de>
2026-04-29 15:13:19 +0200

    updated

--- 0_install_packgs.R
+++ 0_install_packgs.R
@@ -3,6 +3,9 @@
 ## You need to manually install JAGS on your computer: https://sourceforge.net/projects/mcmc-jags/files/
 ## This means you need to download and install locally
 ## If you do not have JAGS installed, R packages will not work
+## You also should have installed JAVA, if this is not the case, you can install it here
+## again manually:
+## https://www.java.com/de/download/
 
 pkgs <- readLines(file("https://raw.githubusercontent.com/hhn365/Theory_Ecotox/refs/heads/main/Lecture_Introduction/installed_pkgs.txt", "r"))
 str(pkgs)
